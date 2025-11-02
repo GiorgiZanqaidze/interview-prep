8216ff2c1be4d84c9d253c568dfcb7a840fde0b0
giorgizanqaidze <giorgizanqaidzeofficial@gmail.com>
2025-11-02 21:44:56 +0400

    change detection

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,11 +1,16 @@
 import { CommonModule } from '@angular/common';
 import {
   ChangeDetectionStrategy,
+  ChangeDetectorRef,
   Component,
-  OnChanges,
-  SimpleChanges,
-  signal,
+  inject,
+  OnInit,
+  Input,
 } from '@angular/core';
+
+interface User {
+  name: string;
+}
 
 @Component({
   selector: 'app-user',
@@ -14,10 +19,61 @@ import {
   imports: [CommonModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class UserComponent implements OnChanges {
-  name = signal('John Doe');
+export class UserComponent implements OnInit {
+  user: User = { name: 'John Doe' };
+  counter = 0;
 
-  ngOnChanges(changes: SimpleChanges): void {
-    console.log(changes);
+  private cdr = inject(ChangeDetectorRef);
+
+  ngOnInit(): void {
+    setInterval(() => {
+      this.user.name = 'New Name' + Math.random(); // mutation
+      console.log('After mutation:', this.user);
+      console.log('Same reference?', true);
+      //   this.cdr.markForCheck();
+    }, 1000);
+  }
+
+  constructor() {
+    // setInterval(() => {
+    //   this.user.name = 'New Name' + Math.random(); // mutation
+    //   console.log('After mutation:', this.user);
+    //   console.log('Same reference?', true);
+    //   this.cdr.markForCheck();
+    // }, 1000);
+  }
+
+  // ❌ Won't trigger change detection
+  mutateUser($event: Event) {
+    $event.preventDefault();
+    console.log('Before mutation:', this.user);
+
+    this.user.name = 'New Name' + Math.random(); // mutation
+    console.log('After mutation:', this.user);
+    console.log('Same reference?', true);
+    // setInterval(() => {
+    //   this.user.name = 'New Name' + Math.random(); // mutation
+    //   console.log('After mutation:', this.user);
+    //   console.log('Same reference?', true);
+    // }, 1000);
+  }
+
+  // ✅ Will trigger change detection
+  replaceUser() {
+    this.user = { ...this.user, name: 'New Name' }; // new reference
+  }
+
+  // ❌ Won't trigger change detection (internal state change)
+  incrementBad() {
+    console.log('Before increment:', this.counter);
+    this.counter++;
+    console.log('After increment:', this.counter);
+    console.log('Same reference?', false);
+  }
+
+  // ✅ Will trigger change detection (with manual trigger)
+  incrementGood() {
+    this.counter++;
+    // this.cdr.markForCheck(); // or cdr.detectChanges()
   }
 }
